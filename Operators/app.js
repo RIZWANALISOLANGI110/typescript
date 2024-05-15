e49aa7a829e5317afba6c0afbdc4ce0f9a17279f
Rizwan Ali <161104718+RIZWANALISOLANGI110@users.noreply.github.com>
2024-05-15 17:59:10 +0500

    Earthematic Operators

---
+++ Operators/app.js
@@ -0,0 +1,20 @@
+// let numberOfFaculty:number = 6;
+// let numberOfManagement: number = 4;
+// let totalNumberOfFaculty = 10
+// console.log(totalNumberOfFaculty + numberOfManagement);
+// let numberOfFaculty:number = 6;
+// let numberOfManagement: number = 4;
+// let totalNumberOfFaculty = 10;
+// console.log(totalNumberOfFaculty - numberOfManagement);
+// let totalMarks: number = 1100;
+// let obtainedMarks = 899;
+// let percentage = 100;
+// console.log(obtainedMarks / totalMarks * percentage);
+var Ali = 10;
+++Ali;
+++Ali;
+--Ali;
+++Ali;
+++Ali;
+--Ali;
+console.log(Ali);
